8a9861f960374341bcc2ba8724e91f994ae5629b
LinaSimon <90833662+LinaSimon@users.noreply.github.com>
2022-03-23 21:51:28 +0100

    Code in progress

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
-// Code from Iwana to get Nodemon dependency
+import express from "express";
+import mongodb from "mongodb";
+import cors from "cors";
+
+// MongoDB setup
 const mongoClient = new mongodb.MongoClient("mongodb://localhost:27017");
 mongoClient.connect();
 const db = mongoClient.db("webshop");
@@ -8,23 +12,33 @@ const collectionOrder = db.Collection("order");
 
 // Creating webserver with node.js and express
 
-const express = require("express");
+// const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
+// const app = express();
+
+// API setup
 const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "localhost://3000"})
+);
+app.use(express.static("public"));
 
+// app.use(
+//  cors({
+//    origin: "http://localhost:3000",
+//  })
+// );
+
+// Message from server
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
-
