d994da9a5f9fc1d67314b78cf31d0eac8f048a6e
LinaSimon <90833662+LinaSimon@users.noreply.github.com>
2022-03-27 19:24:03 +0200

    product api

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import Dog from "./images/dog2.jpg";
 import Unicorn from "./images/unicorn.png";
 import Dino from "./images/dino.png";
 import FirstPage from "./components/FirstPage";
+import { getProducts } from "./api/getProducts";
 // Fetching data from /api endpoint.
 // Making simple GET request using Fetch API to backend
 // and then have data returned as JSON.
 function App() {
   const [data, setData] = React.useState(null);
+  const [products, setProducts] = React.useState(null);
   // HTTP request using useEffect
   React.useEffect(() => {
-    fetch("http://localhost:3001")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    const p = getProducts();
+    setProducts(p);
+    console.log(p);
+    // fetch("http://localhost:3001")
+    //   .then((res) => res.json())
+    //   .then((data) => setData(data.message));
   }, []);
+
+
 
   return (
     <div className="App">
@@ -26,10 +33,13 @@ function App() {
       </header>
       <h1>Webshop</h1>
       <div className="Products">
-        <FirstPage description="Cat" img={Cat} />
+        {products.map(product => {
+          <FirstPage description="Cat" img={Cat} />;
+        })}
+        {/* <FirstPage description="Cat" img={Cat} />
         <FirstPage description="Dog" img={Dog} />
         <FirstPage description="Dino" img={Dino} />
-        <FirstPage description="Unicorn" img={Unicorn} />
+        <FirstPage description="Unicorn" img={Unicorn} /> */}
       </div>
       <div className="CartItems"></div>
     </div>
